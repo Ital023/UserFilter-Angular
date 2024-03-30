5d23439f1f05267b10a25c217cc58f9f5200c46c
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2024-03-29 23:56:21 -0300

    fix: mobile bugs

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -9,7 +9,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 
 
 
