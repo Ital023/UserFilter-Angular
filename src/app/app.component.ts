b4db8920fcb0ba12ba223d2dc81e0a115618c827
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2024-03-30 13:19:22 -0300

    feat: Simulando chamada Http ( Get )

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IUser } from './interfaces/user/user.interface';
+import { UsersList } from './data/users-list';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   userSelected: IUser = {} as IUser;
   showUserDetails: boolean = false;
@@ -15,5 +16,15 @@ export class AppComponent {
       this.userSelected = user;
       this.showUserDetails = true;
   }
+
+  //Simulando uma chamada HTTP (Async)
+  usersList: IUser[] = [];
+
+  ngOnInit(){
+    setTimeout(()=>{
+      this.usersList = UsersList;
+    },1000);
+  }
+
 
 }
