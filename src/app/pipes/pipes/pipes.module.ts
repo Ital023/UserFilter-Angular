97f2293968b48bb9c5739b06d2302541ffed2653
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2024-03-29 22:01:32 -0300

    feat: status pipe

--- src/app/pipes/pipes/pipes.module.ts
+++ src/app/pipes/pipes/pipes.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhonePipe } from './phone.pipe';
 import { AddressPipe } from './address.pipe';
+import { StatusPipe } from './status.pipe';
 
 
 
 @NgModule({
   declarations: [
     PhonePipe,
-    AddressPipe
+    AddressPipe,
+    StatusPipe
   ],
   imports: [
     CommonModule
   ],
   exports:[
     PhonePipe,
-    AddressPipe
+    AddressPipe,
+    StatusPipe
   ]
 })
 export class PipesModule { }
