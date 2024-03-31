5a30dfbbc91d00607fed5e00e586044e766728a3
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2024-03-31 09:06:52 -0300

    feat: filter by name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IUser } from './interfaces/user/user.interface';
 import { UsersList } from './data/users-list';
+import { IFilterOptions } from './interfaces/filter-options.interface';
 
 @Component({
   selector: 'app-root',
@@ -17,14 +18,48 @@ export class AppComponent implements OnInit{
       this.showUserDetails = true;
   }
 
+  onFilter(filterOptions: IFilterOptions){
+      this.userListFiltered = this.filterUsersList(filterOptions, this.usersList);
+  }
+
+  filterUsersList(filterOptions: IFilterOptions, usersList: IUser[]): IUser[] {
+    let filteredList: IUser[] = [];
+
+    filteredList = filterUsersListByName(filterOptions.name,usersList);
+
+    
+
+    return filteredList;
+  }
+
+
+
+
   //Simulando uma chamada HTTP (Async)
   usersList: IUser[] = [];
+  userListFiltered: IUser[] = [];
 
   ngOnInit(){
     setTimeout(()=>{
       this.usersList = UsersList;
+      this.userListFiltered = this.usersList;
     },1000);
   }
 
 
+
 }
+
+
+function filterUsersListByName(name: string | undefined, usersList: IUser[]): IUser[] {
+    const NAME_NOT_TYPPED = name === undefined;
+
+    if(NAME_NOT_TYPPED){
+      return UsersList;
+    }
+
+    const filteredList = usersList.filter((user) => user.nome.toLowerCase().includes(name.toLowerCase()));
+
+    return filteredList;
+}
+
